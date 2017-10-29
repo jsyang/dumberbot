cd6f376f047d913aebf854c0e020bc8f7bcdeb68
jsyang <jsyang.ca@gmail.com>
2017-10-29 20:54:57 +0000

    add web interface and api

---
+++ Strategy/PlacementPhase.ts
@@ -0,0 +1,99 @@
+import Board from '../Board';
+
+let board = new Board();
+const INITIAL_PIECES = {
+    'w': `dd${'w'.repeat(23)}`,
+    'b': `d${'b'.repeat(23)}`
+};
+
+let friendlyColor = 'b';
+let enemyColor = 'w';
+
+let stackRemaining = {
+    w: INITIAL_PIECES.w.split(''),
+    b: INITIAL_PIECES.b.split('')
+};
+
+function reset() {
+    board = new Board();
+    friendlyColor = 'b';
+    enemyColor = 'w';
+    
+    stackRemaining = {
+        w: INITIAL_PIECES.w.split(''),
+        b: INITIAL_PIECES.b.split('')
+    };
+}
+
+const getFreeCells = () => Object.keys(board.state).filter(cell => board.state[cell].length === 0);
+
+const getRandomCellForPlacement = () => {
+    const freeCells = getFreeCells();
+
+    return freeCells[Math.floor(freeCells.length * Math.random())];
+};
+
+const getTotalRemainingMoves = () => stackRemaining.w.length + stackRemaining.b.length;
+
+function doFriendlyMove() {
+    const friendlyStack = stackRemaining[friendlyColor].shift();
+
+    if (friendlyStack) {
+        const move = {
+            stack: friendlyStack,
+            to: getRandomCellForPlacement()
+        };
+
+        console.log(move);
+
+        board.dropStack(move.stack, move.to);
+
+        return move;
+    }
+}
+
+function step(params: any = {}) {
+    const { to } = params;
+
+    if (to) {
+        // Enemy move
+        const enemyStack = stackRemaining[enemyColor].shift();
+        let move;
+
+        if (enemyStack && getFreeCells().indexOf(to) !== -1) {
+            board.dropStack(enemyStack, to);
+            move = { stack: enemyStack, to };
+
+            return [
+                move,
+                doFriendlyMove()
+            ].filter(Boolean);
+        } else {
+            return [];
+        }
+
+    } else {
+        // Friendly move
+        return [
+            doFriendlyMove()
+        ].filter(Boolean);
+    }
+}
+
+export default {
+    reset,
+    getTotalRemainingMoves,
+    setEnemyColor: color => {
+        console.log(`Human is ${color}!`);
+        enemyColor = color;
+
+        if (color === 'b') {
+            friendlyColor = 'w';
+
+            return step();
+        } else {
+            friendlyColor = 'b';
+        }
+    },
+    step
+};
--- web.ts
+++ web.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node_modules/.bin/ts-node
 
 import * as express from 'express';
+import PlacementPhase from './Strategy/PlacementPhase';
 
 const app = express();
 
@@ -8,10 +9,34 @@ const sendFile = file => (
     (req, res) => res.sendFile(`${__dirname}/${file}`)
 );
 
-app.get('/', sendFile('index.html'));
+app.use(express.json());
+
+app.get('/',
+    (req, res, next) => {
+        PlacementPhase.reset();
+        next();
+    },
+    sendFile('index.html')
+);
+
 app.get('/hex.svg', sendFile('hex.svg'));
 
+app.get('/set/human/:color', ({ params }, res) => {
+    const result = PlacementPhase.setEnemyColor(params.color);
 
-// todo: hook api up to bot
+    if (params.color === 'b') {
+        res.json(result);
+    } else {
+        res.json(true);
+    }
+});
 
-app.listen(3000, ()=> console.log('Web server listening on :3000'));
+app.get('/move/:to', ({ params }, res) => {
+    const result = PlacementPhase.step({ to: params.to });
+
+    if (result) {
+        res.json(result);
+    }
+});
+
+app.listen(3000, () => console.log('Web server listening on :3000'));
